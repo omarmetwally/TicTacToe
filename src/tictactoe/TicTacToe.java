2f47ee2af9aac59d7401562e29d3ce143e781e46
Omar <omarmetwally2008@hotmail.com>
2023-12-18 23:26:08 +0200

    adding Splash Screen

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -6,6 +6,7 @@
 package tictactoe;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,6 +14,7 @@ import javafx.stage.Stage;
 import screens.login_screen.LoginScreenBase;
 import screens.LocalPlayers.LocalPlayersBase;
 import screens.LocalOnlinescreen.LocalonscreenBase;
+import screens.SplashScreen.SplashScreenBase;
 import screens.mode.ModeScreenBase;
 
 /**
@@ -20,20 +22,39 @@ import screens.mode.ModeScreenBase;
  * @author Omar
  */
 public class TicTacToe extends Application {
+
     private static Stage primaryStage;
     private static NavigationHistory navHistory = new NavigationHistory();
-    
+
     @Override
     public void start(Stage stage) throws Exception {
 
         this.primaryStage = stage;
-        Parent root = new ModeScreenBase(stage);
+        Parent root = new SplashScreenBase();
         // push awl Screen "Scene"
         Scene initialScene = new Scene(root);
         navHistory.pushScene(initialScene);
         primaryStage.setScene(initialScene);
         primaryStage.setMaximized(true);
         primaryStage.show();
+        new Thread(() -> {
+            try {
+
+                Thread.sleep(2000);
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            }
+
+            Platform.runLater(() -> {
+                try {
+                    ModeScreenBase modeScreen = new ModeScreenBase(stage);
+                    Scene modeScene = new Scene(modeScreen);
+                    changeScene(modeScene);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }).start();
 
     }
 
@@ -43,17 +64,18 @@ public class TicTacToe extends Application {
     }
 
     public static void goBack() {
-        navHistory.popScene(); 
+        navHistory.popScene();
         Scene previousScene = navHistory.peekScene();
         if (previousScene != null) {
             primaryStage.setScene(previousScene);
         }
     }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
