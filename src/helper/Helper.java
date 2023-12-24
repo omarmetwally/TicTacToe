359999065cca156206d504423ddb74f28156b50f
allam <mohamedashraff121197@gmail.com>
2023-12-24 11:10:26 +0200

    finish gson login client

--- src/helper/Helper.java
+++ src/helper/Helper.java
@@ -5,63 +5,42 @@
  */
 package helper;
 
+import com.google.gson.Gson;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.application.Application;
-import javafx.stage.Stage;
 
 /**
  *
  * @author allam
  */
-public class Helper extends Application{
-    
+public class Helper {
+
     Socket playerSocket;
     DataInputStream ear;
     PrintStream mouth;
 
     public Helper() {
-        
-        
 
         try {
             playerSocket = new Socket("127.0.0.1", 5005);
             ear = new DataInputStream(playerSocket.getInputStream());
             mouth = new PrintStream(playerSocket.getOutputStream());
-
-            mouth.println("Test Test");
-            String serverReply = ear.readLine();
-            System.out.println("server says " + serverReply);
-
         } catch (IOException ex) {
             ex.printStackTrace();
-        } 
-        finally {
-
-            try {
-                ear.close();
-                mouth.close();
-                playerSocket.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-
         }
 
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String loginRequest(String userCredential) throws IOException {
+        mouth.println(userCredential);
+        return ear.readLine();
     }
-    
-    public static void main(String [] args){
 
-      new Helper();
+    public static void main(String[] args) {
+
+        new Helper();
     }
-    
+
 }
--- src/screens/Board/BoardFXMLBase.java
+++ src/screens/Board/BoardFXMLBase.java
@@ -12,7 +12,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
